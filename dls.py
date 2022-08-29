89a2905668549d9a5d7ec735f4259a2a33708bc7
Ian H <ianlouishawthorne@gmail.com>
2022-08-29 13:08:18 -0500

    thing

--- dls.py
+++ dls.py
@@ -3,23 +3,26 @@ from pygame.locals import *
 pygame.init()
 
 class inputs(pygame.sprite.Sprite):
-    def __init__(self,x,y) -> None:
+    def __init__(self,x,y,width=113,height=75) -> None:
         super().__init__()
-        self.image = pygame.image.load("imgs/notcopyedplsnosue.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (113, 75)) 
+        self.image = pygame.image.load("imgs/offinput.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (width, height)) 
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.width,self.height = width,height
+    def swap(self):
+        if self.image == pygame.image.load("imgs/offinput.png").convert_alpha():
+            self.image = pygame.image.load("imgs/oninput.png").convert_alpha()
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        else:
+            self.image = pygame.image.load("imgs/oninput.png").convert_alpha()
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
+            
 
-DIM = (2050,1100)
-wn = pygame.display.set_mode((DIM))
+DIM = (1000,750)
+wn = pygame.display.set_mode(DIM, pygame.RESIZABLE)
 pygame.display.set_caption("Digital logic sim by Ianyourgod")
-wn.fill((40,40,40))
-pygame.draw.rect(
-    wn,
-    (20,20,20),
-    (0,0,65,DIM[1])
-    )
 
 FPS_CLOCK = pygame.time.Clock()
 
@@ -27,18 +30,23 @@ inputsdict = {}
 id = 0
 inputsgroup = pygame.sprite.Group()
 
-
 running = True
 while running:
+    w, h = pygame.display.get_surface().get_size()
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
         if event.type == MOUSEBUTTONDOWN:
             mousepos = pygame.mouse.get_pos()
+            clicked_sprites = [s for s in inputsgroup if s.rect.collidepoint(mousepos)]
+            for i in clicked_sprites:
+                i.swap()
             if mousepos[0] < 66:
-                inputsdict[id] = inputs(10,mousepos[1] - 75/2)
+                inputsdict[id] = inputs(10,mousepos[1] - 75/2,70,50)
                 inputsgroup.add(inputsdict[id])
     pygame.display.flip()
+    wn.fill((70,70,70))
+    pygame.draw.rect(wn, (40,40,40), pygame.Rect(0, 0, 65, h))
     inputsgroup.draw(wn)
     FPS_CLOCK.tick(60)
 pygame.quit()
